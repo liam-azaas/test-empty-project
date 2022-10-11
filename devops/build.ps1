df3bd8f6f1a5630619554e3221c6f23b44f92303
liam.wu <liam.wu@azaas.com>
2022-10-11 15:07:39 +0800

    save

--- devops/build.ps1
+++ devops/build.ps1
@@ -2,12 +2,13 @@
 docker info 
  
 $version = $env:VERSION;
-$imageHost = "azinternal-devops-01.southeastasia.cloudapp.azure.com:18000";
+$imageHost = "pkgs.azaas.xyz";
+$imagePrefix = "pkgs.azaas.xyz/test-images";
 
 # build and publish the docker image
 docker login -u $env:DOCKER_USERNAME -p $env:DOCKER_PASSWORD  $imageHost
-docker build -f Dockerfile -t "${imageHost}/test:${version}" .
-docker push "${imageHost}/test:${version}"
+docker build -f Dockerfile -t "${imagePrefix}/test:${version}" .
+docker push "${imagePrefix}/test:${version}"
 
 # remove images from local cache
-docker rmi "${imageHost}/test:${version}"
+docker rmi "${imagePrefix}/test:${version}"
