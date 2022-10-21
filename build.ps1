f03da967b630b40e0cc605ade116b628f2d9961f
liam.wu <liam.wu@azaas.com>
2022-10-21 15:48:25 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,3 +3,5 @@ nuget.exe restore WebApplication1.sln
 
 # build & publish
 msbuild "WebApplication1\WebApplication1.csproj" /p:Configuration=Release -r:False /p:DeployOnBuild=True /p:DeployDefaultTarget=WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=True /p:publishUrl=../output
+
+ls /
